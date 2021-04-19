bbccf4d6df2e748a5410b6f92e91b36e54c858e3
ProdIsMyDev <chris@casualcompany.de>
2021-04-19 02:52:59 +0200

    Testing
    Signed-off-by: ProdIsMyDev <chris@casualcompany.de>

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM python:3.6-alpine
 ADD . /app
 WORKDIR /app
 ENV FLASK_APP=project
-RUN python3 setup.py install
+RUN python setup.py install
 RUN flask init-db
 CMD flask run --host=0.0.0.0
--- setup.py
+++ setup.py
@@ -1,3 +1,35 @@
-from setuptools import setup
+import io
 
-setup()
+from setuptools import find_packages
+from distutils.core import setup
+
+readme = io.open("./README.md", encoding="utf-8").read()
+
+setup(
+    name="project",
+    version="0.0.1",
+    description="Webserver for Covidioten App",
+    long_description=readme,
+    long_description_content_type="text/x-rst",
+    author="Christoph Könning",
+    author_email="chris@casualcompany.de",
+    url="https://github.com/Covidioten/WebServer",
+    license="MIT License",
+    python_requires=">=3.7,<3.8",
+    install_requires=[
+        "flask_cors",
+        "flask_sqlalchemy"
+    ],
+    tests_require=[
+        'pytest >= 3.7.4',
+        'pytest-cov',
+    ],
+    entry_points={
+    },
+    packages=find_packages(
+        exclude=["tests", "tests.*", "examples", "examples.*"]),
+    include_package_data=True,
+    package_data={
+    },
+    zip_safe=False,
+)
